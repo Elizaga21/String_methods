c64adc7c5e4becd33e888881bf030cfbc0a705ff
Elisabet <elisabetaudiovisual@gmail.com>
2023-01-03 01:42:08 +0100

    Initial commit

---
+++ src/ejercicio_2.java
@@ -0,0 +1,31 @@
+
+public class ejercicio_2 {
+    public static void main(String[] args) {
+
+        String s1 = new String("En mi próxima vida, creeré en la reencarnación");
+
+        s1.length();
+        s1.charAt(7);
+        s1.substring(20,27);
+        s1.toUpperCase();
+        s1.indexOf("x");
+        s1.charAt(0);
+
+        System.out.print("Longitud: " + s1.length());
+        System.out.print("\nEn posición 7: " + s1.charAt(7));
+        System.out.print("\nSubcadena:" + s1.substring(20,27));
+        System.out.print("\nPosición del caracter x:" + s1.indexOf("x"));
+        System.out.print("\nEn Mayúsculas:" +  s1.toUpperCase());
+        System.out.print("\nComienza por:" +  s1.charAt(0));
+
+
+
+
+
+
+
+
+
+
+    }
+}
